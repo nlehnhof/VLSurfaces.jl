c8209c7e5eb0a7145647cdb4be61be9ca697a06f
nlehnhof <n.lehnhof01@gmail.com>
2024-08-01 18:01:20 -0600

    Trying to create a basic surface

--- VLSurfaces/src/VLSurfaces.jl
+++ VLSurfaces/src/VLSurfaces.jl
@@ -1,23 +1,52 @@
-module VLSurfaces
+# module VLSurfaces
 
 # Write your package code here.
 using VortexLattice
 
+# wing
+chord1 = [0.2995841993843728, 0.29623678483095345, 0.289425833365632, 0.2788977115682478, 0.26420842943181333, 0.24460947449731052, 0.21878536516659502, 0.18413532299511476, 0.1339781269616617, 0.01]
+xle1 = (chord1[1] / 2) .- (chord1 ./ 2)
+yle1 = range(0.0, 0.75, length(chord1))
+zle1 = zeros(length(chord1))
 
-# Take the basic surface already created -- wing, horizontal stabilizer, vertical stabilizer
-function create_basic_surface(xle::list, yle::list, zle::list, chord::list, ns::Int, nc::Int, spacing_s, spacing_c, mirror::Bool)
+# Reference Values
+Sref = 0.5
+cref = 0.2
+bref = 1.5
+rref = [0.5, 0.0, 0.0]
+Vinf = 1.0
+ref = Reference(Sref, cref, bref, rref, Vinf)
+rho = 1.02
+
+alpha = 5.0*pi/180
+beta = 0.0
+Omega = [0.0; 0.0; 0.0]
+fs = Freestream(Vinf, alpha, beta, Omega)
+
+symmetric = [false, false]
+
+
+# Take the basic surface already created -- wing, horizontal stabilizer, vertical stabilizer, etc.
+
+function create_basic_surface(xle::Vector, yle::Vector, zle::Vector, chord::Vector)
     len = length(chord)
     theta = (2.0*pi/180) * ones(len)
     phi = zeros(len)
     fc = fill((xc) -> 0, len)
     ns = length(chord) - 1
     nc = 6
-    return chord, xle, yle, zle, theta, phi, fc, ns, nc, spacing_s, spacing_c, mirror
+    mirror = true
+    spacing_s = Uniform()
+    spacing_c = Uniform()
+    return chord, xle, yle, zle, theta, phi, ns, nc, mirror, fc, spacing_s, spacing_c
 end
 
 
 # yle_root, yle_tail, xle_root, xle_tail must be index numbers of yle and xle
-function create_control_surface(yle_dist::Float64, yle_root::Int, yle_tail::Int, xle_root::Int, xle_tail::Int, xle::list, yle::list, zle::list, chord::list, theta::list, phi::list, fc, ns::int, nc::int, spacing_s, spacing_c, mirror::Bool)
+# Create control surface and make surface panels
+
+function create_control_surface(yle_dist::Float64, yle_root::Int, yle_tail::Int, xle_root::Int, xle_tail::Int, xle::Vector, yle::Vector, zle::Vector, chord::Vector, theta::Vector, phi::Vector, fc, ns::Int, nc::Int, spacing_s, spacing_c, mirror::Bool)
+    
     # Initialize
     len = length(chord)
 
@@ -39,9 +68,31 @@ function create_control_surface(yle_dist::Float64, yle_root::Int, yle_tail::Int,
     spacing_cc = spacing_c
     mirror_c = mirror 
 
-    return chord_c, xle_c, yle_c, zle_c, theta_c, phi_c, fc_c, ns_c, nc_c, spacing_sc, spacing_cc, mirror_c
+    cgrid, control = wing_to_surface_panels(xle_c, yle_c, zle_c, chord_c, theta_c, phi_c, ns_c, nc_c;
+    mirror=mirror_c, fc = fc_c, spacing_s=spacing_sc, spacing_c=spacing_cc)
+
+    return chord_c, xle_c, yle_c, zle_c, theta_c, phi_c, fc_c, ns_c, nc_c, spacing_sc, spacing_cc, mirror_c, cgrid, control
 end
 
 
+# update original surface and make surface panels
 
+function update_original_surface()
+    chord, xle, yle, zle, theta, phi, fc, ns, nc, spacing_s, spacing_c, mirror = create_basic_surface(xle, yle, zle, chord, ns, nc, spacing_s, spacing_c, mirror)
+    yle_dist_c, yle_root_c, yle_tail_c, xle_root_c, xle_tail_c, xle_c, yle_c, zle_c, chord_c, theta_c, phi_c, fc_c, ns_c, nc_c, spacing_sc, spacing_cc, mirrror_c, cgrid, control = create_control_surface(yle_dist, yle_root, yle_tail, xle_root, xle_tail, xle, yle, zle, chord, theta, phi, fc, ns, nc, spacing_s, spacing_c, mirrror)
+
+    chord_up = []
+    append!(chord_up, chord[1:yle_root_c])
+    append!(chord_up, chord_c)
+    append!(chord_up, chord[yle_tail_c:end])
+
+    mgrid, main = wing_to_surface_panels(xle, yle, zle, chord_up, theta, phi, ns, nc;
+    mirror=mirror, fc = fc, spacing_s=spacing_s, spacing_c=spacing_c)
+
+    return mgrid, main, cgrid, control
 end
+
+
+create_basic_surface(xle1, yle1, zle1, chord1)
+
+# end
